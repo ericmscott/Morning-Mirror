453eba4968f16f8b08a8a4578b5b161886d9ebc2
cmorency <ccmorency@gmail.com>
2017-10-02 13:06:09 -0400

    MVC update

--- MVC/src/Controller.java
+++ MVC/src/Controller.java
@@ -1,4 +1,35 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Controller {
+public class Controller implements ActionListener {
+	int counter;
+	View view;
+	Model model;
+	
+	public Controller (Model model, View view) {
+		this.view = view;
+		this.model = model;
+		view.resetButton.addActionListener(this);
+		view.setButton.addActionListener(this);
+		while (true) {
+			try { Thread.sleep(1000); } catch (Exception e) {};
+			counter++;
+			model.update(counter);	
+		}
+		
+	}
 
+	public void actionPerformed(ActionEvent e) { 
+		if (e.getSource().equals (view.resetButton))
+		{
+			counter = 0;
+		} else if (e.getSource().equals(view.setButton)) {
+			String valueString = view.valueField.getText();
+			try {
+				int value = Integer.parseInt(valueString);
+				counter = value;			
+			} catch (Exception ex) { System.out.println("The text is not a valid integer");}
+		}
+	     
+	}
 }
--- MVC/src/View.java
+++ MVC/src/View.java
@@ -1,4 +1,57 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class View {
+	
+	JButton resetButton;
+	JButton setButton;
+	JTextField counterField, valueField;
+	
+	public View() {
+		
+		JFrame frame = new JFrame("A Simple MVC");
+		Container contentPane = frame.getContentPane();
+		contentPane.setLayout( new BorderLayout());
+		
+		JPanel topPanel = new JPanel();
+		JPanel bottomPanel = new JPanel();
+		contentPane.add( "West", topPanel );
+		contentPane.add ("East", bottomPanel);
+				
+		topPanel.add("West", new JLabel("Counter"));
+		counterField = new JTextField( "0", 10);
+		counterField.setEditable(false);
+		topPanel.add("East",counterField);
+		
+		bottomPanel.setLayout(new BorderLayout());
+		valueField = new JTextField( 10);
+		counterField.setEditable(true);
+		bottomPanel.add("North",valueField);
+		JPanel buttonPanel = new JPanel();
+		bottomPanel.add("South",buttonPanel);
+		buttonPanel.setLayout(new FlowLayout());
+		resetButton = new JButton ("RESET");
+		buttonPanel.add(resetButton);
+		buttonPanel.add(resetButton);
+		setButton = new JButton ("SET");
+		buttonPanel.add(setButton);
+		buttonPanel.add(setButton);
 
+		frame.setSize(350,100);
+		frame.setLocation(100,100);
+		frame.setVisible(true);
+		
+	}
+	
+	public void update(int counter) {
+		counterField.setText(Integer.toString(counter));
+		return;
+	}
 }
